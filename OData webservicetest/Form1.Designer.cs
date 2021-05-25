4f72ff85207d3c90f21b77784f44b068e3ac6e45
admin <admin@DESKTOP-6SFHLFD>
2021-05-25 09:12:40 +0300

    new changes with comment

--- OData webservicetest/Form1.Designer.cs
+++ OData webservicetest/Form1.Designer.cs
@@ -29,10 +29,10 @@
         private void InitializeComponent()
         {
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
+            this.Read_Data = new System.Windows.Forms.Button();
+            this.Insert_Data = new System.Windows.Forms.Button();
+            this.Edite_Data = new System.Windows.Forms.Button();
+            this.Delete_Data = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -44,55 +44,55 @@
             this.dataGridView1.Size = new System.Drawing.Size(605, 216);
             this.dataGridView1.TabIndex = 0;
             // 
-            // button1
+            // Read_Data
             // 
-            this.button1.Location = new System.Drawing.Point(535, 246);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(96, 31);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "publish";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.Read_Data.Location = new System.Drawing.Point(535, 246);
+            this.Read_Data.Name = "Read_Data";
+            this.Read_Data.Size = new System.Drawing.Size(96, 31);
+            this.Read_Data.TabIndex = 1;
+            this.Read_Data.Text = "Load data";
+            this.Read_Data.UseVisualStyleBackColor = true;
+            this.Read_Data.Click += new System.EventHandler(this.button1_Click);
             // 
-            // button2
+            // Insert_Data
             // 
-            this.button2.Location = new System.Drawing.Point(12, 253);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(101, 24);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "added";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.Insert_Data.Location = new System.Drawing.Point(12, 253);
+            this.Insert_Data.Name = "Insert_Data";
+            this.Insert_Data.Size = new System.Drawing.Size(101, 24);
+            this.Insert_Data.TabIndex = 2;
+            this.Insert_Data.Text = "added";
+            this.Insert_Data.UseVisualStyleBackColor = true;
+            this.Insert_Data.Click += new System.EventHandler(this.button2_Click);
             // 
-            // button3
+            // Edite_Data
             // 
-            this.button3.Location = new System.Drawing.Point(155, 254);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(116, 23);
-            this.button3.TabIndex = 3;
-            this.button3.Text = "Modify";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.Edite_Data.Location = new System.Drawing.Point(155, 254);
+            this.Edite_Data.Name = "Edite_Data";
+            this.Edite_Data.Size = new System.Drawing.Size(116, 23);
+            this.Edite_Data.TabIndex = 3;
+            this.Edite_Data.Text = "Modify";
+            this.Edite_Data.UseVisualStyleBackColor = true;
+            this.Edite_Data.Click += new System.EventHandler(this.button3_Click);
             // 
-            // button4
+            // Delete_Data
             // 
-            this.button4.Location = new System.Drawing.Point(314, 253);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 23);
-            this.button4.TabIndex = 4;
-            this.button4.Text = "Delete";
-            this.button4.UseVisualStyleBackColor = true;
-            this.button4.Click += new System.EventHandler(this.button4_Click);
+            this.Delete_Data.Location = new System.Drawing.Point(314, 253);
+            this.Delete_Data.Name = "Delete_Data";
+            this.Delete_Data.Size = new System.Drawing.Size(75, 23);
+            this.Delete_Data.TabIndex = 4;
+            this.Delete_Data.Text = "Delete";
+            this.Delete_Data.UseVisualStyleBackColor = true;
+            this.Delete_Data.Click += new System.EventHandler(this.button4_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(659, 308);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Delete_Data);
+            this.Controls.Add(this.Edite_Data);
+            this.Controls.Add(this.Insert_Data);
+            this.Controls.Add(this.Read_Data);
             this.Controls.Add(this.dataGridView1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -104,10 +104,10 @@
         #endregion
 
         private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button Read_Data;
+        private System.Windows.Forms.Button Insert_Data;
+        private System.Windows.Forms.Button Edite_Data;
+        private System.Windows.Forms.Button Delete_Data;
     }
 }
 
--- OData webservicetest/Form1.cs
+++ OData webservicetest/Form1.cs
@@ -20,6 +20,7 @@ namespace OData_webservicetest
 
         private void button1_Click(object sender, EventArgs e)
         {
+            //Read data from business central
             Odataservice.NAV nav = new Odataservice.NAV(new Uri("http://localhost:7048/BC140/OData/Company('CRONUS%20International%20Ltd.')"));
             nav.Credentials = CredentialCache.DefaultNetworkCredentials;
             var customers = from c in nav.my_customer_service select c;
@@ -28,6 +29,7 @@ namespace OData_webservicetest
 
         private void button2_Click(object sender, EventArgs e)
         {
+            //Insert data  from business central
             Odataservice.NAV nav = new Odataservice.NAV(new Uri("http://localhost:7048/BC140/OData/Company('CRONUS%20International%20Ltd.')"));
             nav.Credentials = CredentialCache.DefaultNetworkCredentials;
             var customers = new Odataservice.my_customer_service
@@ -42,11 +44,12 @@ namespace OData_webservicetest
 
         private void button3_Click(object sender, EventArgs e)
         {
+            //Update date from business central
             Odataservice.NAV nav = new Odataservice.NAV(new Uri("http://localhost:7048/BC140/OData/Company('CRONUS%20International%20Ltd.')"));
             nav.Credentials = CredentialCache.DefaultNetworkCredentials;
             var cusatomers = nav.my_customer_service.Where(c => c.No == "C00010").First();
             cusatomers.Credit_Limit_LCY = 20000;
-            cusatomers.Name = "Myodata Demo";
+            cusatomers.Name = "Myodata Demos";
             // cusatomers.Address = "boles";
             nav.UpdateObject(cusatomers);
             nav.SaveChanges();
@@ -54,6 +57,7 @@ namespace OData_webservicetest
 
         private void button4_Click(object sender, EventArgs e)
         {
+            //Delete data from business central
             Odataservice.NAV nav = new Odataservice.NAV(new Uri("http://localhost:7048/BC140/OData/Company('CRONUS%20International%20Ltd.')"));
             nav.Credentials = CredentialCache.DefaultNetworkCredentials;
             var cusatomers = nav.my_customer_service.Where(c => c.No == "C00010").First();
